ef6335d917b731f44d72acf3646ca85288eba812
Daichi Onda <daichi.onda@gmail.com>
2017-04-20 00:38:25 -0400

    changing to daichi's google project

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,10 +4,10 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, 
 
   # YOUR CLIENT ID
-  '262701678261-gdtc3v25vjt01dfu860mdma4hj566sk4.apps.googleusercontent.com', 
+  '636361556889-1hub2nps8t11tjnju51qc4fif0f14a2k.apps.googleusercontent.com', 
   
   # YOUR CLIENT SECRET
-  'ZCBU1nBy0mrEmsjG5rF8B--5', 
+  'Ldojff143lIHSMZegPDDkX5C', 
   {
   	client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}, 
 
