1ba415f06ab0772e6a83ed9abcb2b7835497ed36
Daichi Onda <daichi.onda@gmail.com>
2017-04-20 00:15:24 -0400

    fix?

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
-OmniAuth.config.full_host = Rails.env.production? ? 'https://limitless-taiga-46118.herokuapp.com/' : 'http://localhost:3000'
+OmniAuth.config.full_host = Rails.env.production? ? 'https://limitless-taiga-46118.herokuapp.com' : 'http://localhost:3000'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, 
